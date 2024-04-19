621e40538e23259939c2c8f1adf8f712c2fd20ca
Shantanu Singh <shsingh@broadinstitute.org>
2024-04-18 21:16:06 -0400

    Executable

---
+++ download_data_stem.sh
@@ -0,0 +1,5 @@
+mkdir -p inputs/workspace/profiles/stem/
+
+cp \
+  "/Users/shsingh/Documents/GitHub/neuronal-cell-painting/5.reanalysis/output/processed/augmented/combined.parquet" \
+  "inputs/workspace/profiles/stem/profiles.parquet"
